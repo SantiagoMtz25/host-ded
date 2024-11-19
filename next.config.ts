179e682fa7c73d89942ffee30a5b7be17d7e600d
SantiagoMtz25 <santimtzv01@gmail.com>
2024-11-19 17:15:17 -0600

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/CCA_CVA/profesores/cursos/hb028/becario',
+  assetPrefix: '/CCA_CVA/profesores/cursos/hb028/becario',
 };
 
 export default nextConfig;
